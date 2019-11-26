b54c1a718e3f9e11e3ef9b79afa7322cd0114b71
farrert <farrert@mail.nmc.edu>
2019-11-26 18:09:10 -0500

    Add files via upload

---
+++ CapStone/CapStone/Models/Box.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static CapStone.ViewModels.GameViewModel;
+
+namespace CapStone.Models
+{
+    public class Box : ObservableObject
+    {
+        #region Enum
+
+            public enum BoxState { Empty, X, O }
+            private Dictionary<string, BoxState> CurrentBox = new Dictionary<string, BoxState>()
+        {
+            { "", BoxState.Empty },
+            { "X", BoxState.X },
+            { "O", BoxState.O }
+        };
+            #endregion
+            #region Fields
+
+            private bool _checked;
+            private int _id;
+            private BoxState _state;
+
+        #endregion
+        #region PROPERTIES
+
+        public bool Checked
+        {
+            get { return _checked; }
+            set { _checked = value; }
+        }
+
+        public int Id
+            {
+                get { return _id; }
+                set
+                {
+                    _id = value;
+                }
+            }
+            public BoxState State
+            {
+                get { return _state; }
+                set
+                {
+                    _state = value;
+                    OnPropertyChanged(nameof(BoxState));
+                }
+            }
+        #endregion
+        #region Constructors
+        public Box()
+        {
+
+        }
+        #endregion
+    }
+}
---
+++ CapStone/CapStone/ViewModels/GameViewModel.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using CapStone.Models;
+using static CapStone.Models.Box;
+
+namespace CapStone.ViewModels
+{
+    public class GameViewModel : ObservableObject
+    {
+        #region Commands
+
+        public ICommand BoxChangeCommand { get; set; }
+        public ICommand GameLogicCommand { get; set; }
+
+        #endregion
+        #region  Fields
+
+        private ObservableCollection<Box> _board;
+        private BoxState _getState;
+        private Box _currentBox;
+        public bool Checked = false;
+
+        #endregion
+        #region Properties
+
+        public Box BoxChange
+        {
+             get; set; 
+        }
+        //public ObservableCollection<Box> Board
+        //{
+        //    get { return _board; }
+        //    set { _board = value; }
+        //}
+        private Box CurrentBox { get; set; }
+        public BoxState GetState
+        {
+            get { return _getState; }
+            set
+            {
+                _getState = value;
+                // PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("X"));
+            }
+        }
+        #endregion
+        private string _displayContent = "";
+        
+
+        public string DisplayContent
+        {
+            get { return _displayContent; }
+            set
+            {
+                _displayContent = value;
+                OnPropertyChanged("DisplayContent");
+            }
+        }
+        public GameViewModel()
+        {
+            InitializeViewModel();
+        }
+        private void InitializeViewModel() => BoxChangeCommand = new RelayCommand(new Action<object>(GetUpdateBox()));
+        //GameLogicCommand = new RelayCommand(new Action(CheckConditions));
+
+        //private void UpdateBox(object obj)
+        //{
+        //    if (obj.ToString() == "X")
+        //    {
+        //        DisplayContent = obj.ToString();
+        //    }
+        //    else DisplayContent = "X";
+        //}
+            public Action<object> GetUpdateBox()
+        {
+            return UpdateBox;
+        }
+
+        private void UpdateBox(Object obj)
+        {
+            _currentBox = new Box();
+            _currentBox.State = BoxState.X;
+            OnPropertyChanged("DisplayContent");
+            _displayContent = "X";
+            if (obj.ToString() != "X" || obj.ToString() != "O")
+            {
+                obj = "X";
+
+            }
+        }
+        private void OnBoxChange()
+        {
+            if (_currentBox == null)
+            {
+                _currentBox = new Box();
+                _currentBox.State = BoxState.X;
+               // PropertyChanged("BoxChange");
+            }
+        }
+        //private void Button_Click(object sender, RoutedEventArgs e) => UpdateBox(this);
+        private void CheckConditions(object obj)
+        {
+
+        }
+    }
+
+
+}
